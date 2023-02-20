32555dc236615c5bb59ee272fb42e0ae793a9ee0
Daniel Gabriel <reelpscanal@gmail.com>
2023-02-20 15:53:26 -0300

    api key change

--- BackEnd/src/libs/axios.ts
+++ BackEnd/src/libs/axios.ts
@@ -1,5 +1,5 @@
-import axios from "axios";
+import axios from "axios"
 
 export const api = axios.create({
-    baseURL:`https://api.spoonacular.com/mealplanner/generate?apiKey=98b5f30ea5ae4f4184ee89f8e7cd4c89&timeFrame=day`
+  baseURL: `https://api.spoonacular.com/mealplanner/generate?apiKey=7aa5c685ad9c49eb949ce9f8264c62b4&timeFrame=day`,
 })
